1dfba86858bb1d38122eb4730b6674890150e434
m.kalenishin <m.kalenishin@ylab.io>
2022-09-20 20:50:37 +0300

    feat: added Bison and BisonTest classes

---
+++ src/ZooLabLibrary/Animals/Mammals/Bison.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooLabLibrary.Animals.Medicines;
+using ZooLabLibrary.Animals.Foods;
+
+namespace ZooLabLibrary.Animals.Mammals
+{
+    public class Bison : Mammal
+    {
+        private static readonly string[] _friendlyAnimals = new string[]
+{
+                "Elephant",
+                "Bison"
+};
+
+        public Bison() { }
+        public Bison(int iD) : base(iD) { }
+        public Bison(int iD, bool isSick) : base(iD, isSick)
+        {
+            IsSick = isSick;
+        }
+
+        public Bison(int iD, bool isSick, bool isHungry) : base(iD, isSick, isHungry)
+        {
+            IsSick = isSick;
+            IsHungry = isHungry;
+        }
+
+        public override int RequiredSpaceSqFt { get; } = 1000;
+        public override string[] FavouriteFood { get; } = new string[] { "Grass" };
+
+        public override bool IsSick { get; set; }
+
+
+
+        public override bool IsFriendlyWith(Animal animal)
+        {
+            return _friendlyAnimals.Contains(animal.GetType().Name);
+        }
+
+        public override void Feed(Food food)
+        {
+            if (IsHungry == true && FavouriteFood.Contains(food.FoodType))
+                IsHungry = false;
+        }
+
+        public override void Heal(Medicine medicine)
+        {
+            if (this.IsSick == true && medicine.MedicineType == "Antibiotics")
+                IsSick = false;
+        }
+    }
+}
---
+++ tests/ZooLabLibrary.Test/Animals/Mammals/BisonTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooLabLibrary.Animals.Birds;
+using ZooLabLibrary.Animals.Foods;
+using ZooLabLibrary.Animals.Mammals;
+using ZooLabLibrary.Animals.Medicines;
+
+namespace ZooLabLibrary.Test.Animals.Mammals
+{
+    public class BisonTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateBison()
+        {
+            Bison bison = new Bison();
+            Assert.NotNull(bison);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToCreateSickBison()
+        {
+            Bison bison = new Bison(iD: 0, isSick: true);
+            Assert.NotNull(bison);
+            Assert.True(bison.IsSick);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToCreateSickAndHungryBison()
+        {
+            Bison bison = new Bison(iD: 0, isSick: true, isHungry: true);
+            Assert.NotNull(bison);
+            Assert.True(bison.IsSick);
+            Assert.True(bison.IsHungry);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToReturnRequiredSpaceSqFt()
+        {
+            Bison bison = new Bison(0);
+            var requiredSpaceSqFt = bison.RequiredSpaceSqFt;
+            Assert.Equal(1000, requiredSpaceSqFt);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToReturnFavouriteFood()
+        {
+            Bison bison = new Bison(0);
+            var favouriteFood = bison.FavouriteFood;
+            Assert.Contains("Grass", favouriteFood);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAssessIsAnimalFriendlyOrNot()
+        {
+            Bison bison1 = new Bison(0);
+            Bison bison2 = new Bison(1);
+            Lion lion = new Lion(2);
+            Penguin penguin = new Penguin(3);
+            Assert.False(bison1.IsFriendlyWith(lion));
+            Assert.False(bison1.IsFriendlyWith(penguin));
+            Assert.True(bison1.IsFriendlyWith(bison2));
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToFeedBison()
+        {
+            Bison bison = new Bison(0);
+            bison.IsHungry = true;
+            Meat pieceOfMeat = new Meat();
+            bison.Feed(pieceOfMeat);
+            Assert.True(bison.IsHungry);
+            Grass grass = new Grass();
+            bison.Feed(grass);
+            Assert.False(bison.IsHungry);
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToAddScheduale()
+        {
+            Bison bison = new Bison(iD: 0, isSick: true);
+            var hours = new List<int>() { 10, 15 };
+            bison.AddScheduale(hours);
+            Assert.Contains(10, bison.FeedScheduale);
+            Assert.Contains(15, bison.FeedScheduale);
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToHealBison()
+        {
+            Bison bison = new Bison(iD: 0, isSick: true);
+            var antiDepression = new AntiDepression();
+            bison.Heal(antiDepression);
+            Assert.True(bison.IsSick);
+            var antibiotics = new Antibiotics();
+            bison.Heal(antibiotics);
+            Assert.False(bison.IsSick);
+        }
+    }
+}
