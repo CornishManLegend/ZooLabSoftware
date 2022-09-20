a471b5e1f72641dfb957e4fbe0a9d001434a09bf
m.kalenishin <m.kalenishin@ylab.io>
2022-09-20 21:07:52 +0300

    feat: added Elephant and ElefantTest classes

---
+++ src/ZooLabLibrary/Animals/Mammals/Elephant.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooLabLibrary.Animals.Foods;
+using ZooLabLibrary.Animals.Medicines;
+
+namespace ZooLabLibrary.Animals.Mammals
+{
+    public class Elephant : Mammal
+    {
+        private static readonly string[] _friendlyAnimals = new string[]
+{
+                "Elephant",
+                "Bison",
+                "Parrot",
+                "Turtle"
+};
+
+        public Elephant() { }
+        public Elephant(int iD) : base(iD) { }
+        public Elephant(int iD, bool isSick) : base(iD, isSick)
+        {
+            IsSick = isSick;
+        }
+
+        public Elephant(int iD, bool isSick, bool isHungry) : base(iD, isSick, isHungry)
+        {
+            IsSick = isSick;
+            IsHungry = isHungry;
+        }
+
+        public override int RequiredSpaceSqFt { get; } = 1000;
+        public override string[] FavouriteFood { get; } = new string[] { "Grass", "Vegetable" };
+
+        public override bool IsSick { get; set; }
+
+
+
+        public override bool IsFriendlyWith(Animal animal)
+        {
+            return _friendlyAnimals.Contains(animal.GetType().Name);
+        }
+
+        public override void Feed(Food food)
+        {
+            if (IsHungry == true && FavouriteFood.Contains(food.FoodType))
+                IsHungry = false;
+        }
+
+        public override void Heal(Medicine medicine)
+        {
+            if (this.IsSick == true && medicine.MedicineType == "Antibiotics")
+                IsSick = false;
+        }
+    }
+}
---
+++ tests/ZooLabLibrary.Test/Animals/Mammals/ElephantTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooLabLibrary.Animals.Birds;
+using ZooLabLibrary.Animals.Foods;
+using ZooLabLibrary.Animals.Mammals;
+using ZooLabLibrary.Animals.Medicines;
+
+namespace ZooLabLibrary.Test.Animals.Mammals
+{
+    public class ElephantTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateElephant()
+        {
+            Elephant elephant = new Elephant();
+            Assert.NotNull(elephant);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToCreateSickElephant()
+        {
+            Elephant elephant = new Elephant(iD: 0, isSick: true);
+            Assert.NotNull(elephant);
+            Assert.True(elephant.IsSick);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToCreateSickAndHungryElephant()
+        {
+            Elephant elephant = new Elephant(iD: 0, isSick: true, isHungry: true);
+            Assert.NotNull(elephant);
+            Assert.True(elephant.IsSick);
+            Assert.True(elephant.IsHungry);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToReturnRequiredSpaceSqFt()
+        {
+            Elephant elephant = new Elephant(0);
+            var requiredSpaceSqFt = elephant.RequiredSpaceSqFt;
+            Assert.Equal(1000, requiredSpaceSqFt);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToReturnFavouriteFood()
+        {
+            Elephant elephant = new Elephant(0);
+            var favouriteFood = elephant.FavouriteFood;
+            Assert.Contains("Grass", favouriteFood);
+            Assert.Contains("Vegetable", favouriteFood);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAssessIsAnimalFriendlyOrNot()
+        {
+            Elephant elephant1 = new Elephant(0);
+            Elephant elephant2 = new Elephant(1);
+            Lion lion = new Lion(2);
+            Penguin penguin = new Penguin(3);
+            Parrot parrot = new Parrot(4);
+            Assert.False(elephant1.IsFriendlyWith(lion));
+            Assert.False(elephant1.IsFriendlyWith(penguin));
+            Assert.True(elephant1.IsFriendlyWith(elephant2));
+            Assert.True(elephant1.IsFriendlyWith(parrot));
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToFeedElephant()
+        {
+            Elephant elephant = new Elephant(0);
+            elephant.IsHungry = true;
+            Meat pieceOfMeat = new Meat();
+            elephant.Feed(pieceOfMeat);
+            Assert.True(elephant.IsHungry);
+            Grass grass = new Grass();
+            elephant.Feed(grass);
+            Assert.False(elephant.IsHungry);
+            elephant.IsHungry = true;
+            Vegetable vegetable = new Vegetable();
+            elephant.Feed(vegetable);
+            Assert.False(elephant.IsHungry);
+
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToAddScheduale()
+        {
+            Elephant elephant = new Elephant(iD: 0, isSick: true);
+            var hours = new List<int>() { 10, 15 };
+            elephant.AddScheduale(hours);
+            Assert.Contains(10, elephant.FeedScheduale);
+            Assert.Contains(15, elephant.FeedScheduale);
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToHealElephant()
+        {
+            Elephant elephant = new Elephant(iD: 0, isSick: true);
+            var antiDepression = new AntiDepression();
+            elephant.Heal(antiDepression);
+            Assert.True(elephant.IsSick);
+            var antibiotics = new Antibiotics();
+            elephant.Heal(antibiotics);
+            Assert.False(elephant.IsSick);
+        }
+    }
+}
