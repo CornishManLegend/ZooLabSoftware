13f572f6b3bd9b0770a3d8866ffdd31fe818b5e9
m.kalenishin <m.kalenishin@ylab.io>
2022-09-22 03:00:21 +0300

    feat: added ZooAppTest

---
+++ tests/ZooLabLibrary.Test/ZooAppTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooLabLibrary.Test
+{
+    public class ZooAppTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateZooApp()
+        {
+            ZooApp zooApp = new ZooApp();
+            Assert.NotNull(zooApp);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToAddZooToZooAppAndGetZoos()
+        {
+            ZooApp zooApp = new ZooApp();
+            zooApp.AddZoo(new Zoo(location: "New York"));
+            Assert.NotEmpty(zooApp.GetZoos());
+        }
+    }
+}
